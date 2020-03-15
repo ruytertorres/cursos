530a93cc9befbab6ed53fbecac07b3b7e724aa88
Ruyter Torres <ruytertorres@gmail.com>
2020-03-14 22:29:50 -0300

    Atualização 14/03/2020 22:30 - Eventos pt1

--- 4. JavaScript/Aula 23 - Eventos (teclado)/js/scriptone.js
+++ 4. JavaScript/Aula 23 - Eventos (teclado)/js/scriptone.js
@@ -1,6 +1,6 @@
 // Evento Mouse
 
 function acao(){
-    //alert('Teste bem sucedido!!!')
-    document.write('<p>Teste bem sucedido!!!</p>' + '<br><br>' + '<a href="index.html"><< Voltar</a>')
+    alert('Teste bem sucedido!!!')
+    //document.write('<p>Teste bem sucedido!!!</p>' + '<br><br>' + '<a href="index.html"><< Voltar</a>')
 }
---
+++ 4. JavaScript/Aula 24 - Eventos (Frame)/js/scriptone.js
@@ -0,0 +1,7 @@
+// Evento Frame
+
+function acao(){
+    //alert('Teste bem sucedido!!!')
+    // document.write('<p>Teste bem sucedido!!!</p>' + '<br><br>' + '<a href="index.html"><< Voltar</a>')
+    console.log('Evento Disparado')
+}
