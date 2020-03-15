8e547a7ec0415f2baf554b1877aedcc7b1f7a641
Ruyter Torres <ruytertorres@gmail.com>
2020-03-15 13:32:24 -0300

    Atualizção 15/03/2020 13:33 - DOM parte 1

---
+++ 4. JavaScript/Aula 26 e 27 - DOM selecionando e manipulando os elementos da pagina/js/scriptone.js
@@ -0,0 +1,20 @@
+// Seleção de Elementos
+
+// document.getElementById()
+
+console.log(document.getElementById('username'))
+
+// getElementsByTagName
+
+console.log(document.getElementsByTagName('input'))
+
+// getElementsByClassName
+
+console.log(document.getElementsByClassName('textarea'))
+
+// getElementsByName
+
+console.log(document.getElementsByName('nome'))
+
+//Prática
+
