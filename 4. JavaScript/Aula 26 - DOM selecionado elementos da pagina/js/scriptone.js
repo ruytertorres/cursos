a13d05fb313642a7cf08ff1887b750f005dd9165
Ruyter Torres <ruytertorres@gmail.com>
2020-03-15 00:57:51 -0300

    Atualização 15/03/2020 00:58 - DOM pt1

---
+++ 4. JavaScript/Aula 26 - DOM selecionado elementos da pagina/js/scriptone.js
@@ -0,0 +1,17 @@
+// Seleção de Elementos
+
+// document.getElementById()
+
+console.log(document.getElementById('username'))
+
+// getElementsByTagName
+
+console.log(document.getElementsByTagName('input'))
+
+// getElementsByClassName
+
+console.log(document.getElementsByClassName('textarea'))
+
+// getElementsByName
+
+console.log(document.getElementsByName('nome'))
