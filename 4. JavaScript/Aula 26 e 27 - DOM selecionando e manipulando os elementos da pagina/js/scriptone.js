2086ca218168a0efd8ee73e8688872dae1eab2ff
Ruyter Torres <ruytertorres@gmail.com>
2020-03-17 00:39:04 -0300

    Atuaização 17/03/2020 00:40 - Atividade de Fixação

--- 4. JavaScript/Aula 26 e 27 - DOM selecionando e manipulando os elementos da pagina/js/scriptone.js
+++ 4. JavaScript/Aula 26 e 27 - DOM selecionando e manipulando os elementos da pagina/js/scriptone.js
@@ -6,7 +6,7 @@ console.log(document.getElementById('username'))
 
 // getElementsByTagName
 
-console.log(document.getElementsByTagName('input'))
+//console.log(document.getElementsByTagName('input'))
 
 // getElementsByClassName
 
@@ -18,3 +18,28 @@ console.log(document.getElementsByName('nome'))
 
 //Prática
 
+function distribuirCaracter(){
+    var caracter = document.getElementById('entrada').value
+    document.getElementById('entrada').value = ""
+
+    console.log(caracter)
+
+    caracter.trim()
+
+    switch(caracter){
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            document.getElementById('numeros').value = caracter
+            break
+        default:
+            document.getElementById('letras').value += caracter // Concatenado a logica da varivel com a propria variavel, o campo de texto n é resetado, diferrente do numero onde não houve concatenação
+        }
+}
---
+++ 4. JavaScript/Aula 29 - DOM Manipulando class/js/scriptone.js
@@ -0,0 +1,81 @@
+// Manipulando estilo de elemento
+/*
+function modificarEstilo (backGround, circunference){
+    document.getElementById('retornoDeId').style.background = backGround
+    document.getElementById('retornoDeId').style.borderRadius = circunference
+}
+*/
+
+// Manipulando Class
+
+function modificaClass (){
+    document.getElementById('retorno').className = "est-1"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Seleção de Elementos
+
+// document.getElementById()
+
+console.log(document.getElementById('username'))
+
+// getElementsByTagName
+
+//console.log(document.getElementsByTagName('input'))
+
+// getElementsByClassName
+
+console.log(document.getElementsByClassName('textarea'))
+
+// getElementsByName
+
+console.log(document.getElementsByName('nome'))
+
+//Prática
+
+function distribuirCaracter(){
+    var caracter = document.getElementById('entrada').value
+    document.getElementById('entrada').value = ""
+
+    caracter.trim()
+
+    switch(caracter){
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            document.getElementById('numeros').value = caracter
+            break
+        default:
+            document.getElementById('letras').value += caracter // Concatenado a logica da varivel com a propria variavel, o campo de texto n é resetado, diferrente do numero onde não houve concatenação
+        }
+}
+*/
