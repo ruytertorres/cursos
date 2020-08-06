5949583794e268c96f8af6ba605dfda780da2c11
Ruyter Torres <ruytertorres@gmail.com>
2020-08-06 16:40:37 -0300

    Atualização de transferencia - 06/08/2020 16:42

--- 4. JavaScript/1. JavaScript Matriz/Aula 47 - BOM Windows/js/scriptone.js
+++ 4. JavaScript/1. JavaScript Matriz/Aula 47 - BOM Windows/js/scriptone.js
@@ -1,34 +1,13 @@
-// Tratamento de erros
+// BOM
 
-// throw
+//consultar documentação w3School
 
-//throw new Error('Houve um erro, mas nossa equipe está de olho nisso. Logo tudo estará normal para você!!!')
+var janelaPoupUpGoogle
 
-//netflix
-
-var video = Array()
-
-video[1] = Array()
-video[1]['nome'] = 'Fullmetal Alchemist'
-video[1]['categoria'] = 'Anime'
-
-function getVideo(video) {
-
-    try{
-    console.log(video[0]['nome'])
-    } catch(erro){
-        tratarErro(erro)
-        console.log('Aqui se trata o error / mensagem de erro')
-        throw new Error('Houve um erro, mas nossa equipe está de olho nisso. Logo tudo estará normal para você!!!')
-    } finally {
-        console.log('Sempre passa por aqui (try / catch)')
-    }
-
-    console.log('A aplicação não morreu') 
+function openPopUp(){
+    janelaPoupUpGoogle = window.open('http://www.google.com', 'new_window', 'width=600, height=300')
 }
 
-function tratarErro(e){
-    // logica para tratar o registrar o erro no servidor
-    console.log(e)
+function closePopUp(){
+    janelaPoupUpGoogle.close()
 }
-getVideo(video)
