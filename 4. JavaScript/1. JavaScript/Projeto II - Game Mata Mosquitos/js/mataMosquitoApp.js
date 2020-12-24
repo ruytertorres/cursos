a57b5ef7cbe4254735c50dd6a12ee008445b8861
ruytertorres <ruytertorres@gmail.com>
2020-12-24 02:25:15 +0000

    2:24(NY) 24/12/2020 - Atualização 2 app mata mosquito

--- 4. JavaScript/1. JavaScript/Projeto II - Game Mata Mosquitos/js/mataMosquitoApp.js
+++ 4. JavaScript/1. JavaScript/Projeto II - Game Mata Mosquitos/js/mataMosquitoApp.js
@@ -1,17 +1,42 @@
 /* appMataMosquito.js */
 
-var alt = 0;
-var larg = 0;
+var alt = 0
+var larg = 0
 
 function ajustaTamanhoPalcoJogo(){
 
-alt = "altura: " + window.innerHeight + "px ||" // recupera altura do browser
-larg = "largura: " + window.innerWidth + "px" // recupera largura do browser
+alt = window.innerHeight // recupera altura do browser
+larg = window.innerWidth // recupera largura do browser
 
-console.log(alt, larg)
+console.log("altura: " + alt + "px || " + "largura: " + larg + "px")
 }
 
 ajustaTamanhoPalcoJogo() // chamada da função
 
 // o trecho de código acima é para tornar dinamico o palco do jogo permitindo que as aparições randomica se de somente na area visivel do browser, independete de redimencionamento do mesmo
 
+function positionMoscaRandom(){
+    
+    let positionX = Math.floor(Math.random() * larg) - 90
+    let positionY = Math.floor(Math.random() * alt) - 90
+
+    /* O trecho acima visa programar de forma randomica a posição de onde as moscas aparecerão */
+
+    /* Tratamento de erro da posição 0, usando um operador ternário */
+
+    positionX = positionX < 0 ? 0 : positionX    
+    positionY = positionY < 0 ? 0 : positionY    
+
+    console.log("X: " + positionX + " || " + "Y: " + positionY)
+
+    // Criar elemento HTML usando o DOM
+
+    let mosquito = document.createElement('img')
+    mosquito.src = 'img/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = positionX + 'px'
+    mosquito.style.top = positionY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+} 
